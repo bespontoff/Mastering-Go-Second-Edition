d19bc5a093f805a92a6478c82b3d3b1293d9beb6
bespontoff <bespontoff@gmail.com>
2021-02-02 11:01:44 +0300

    sum of args

--- ch01/practice/num_adder.go
+++ ch01/practice/num_adder.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -11,5 +12,13 @@ func main() {
 		os.Exit(1)
 	}
 	arguments := os.Args
-	fmt.Println(arguments)
+	sum := 0.0
+	for i := 0; i < len(arguments); i++ {
+		n, err := strconv.ParseFloat(arguments[i], 64)
+
+		if err == nil {
+			sum += n
+		}
+	}
+	fmt.Println("Sum of args =", sum)
 }
